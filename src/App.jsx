c4ed73442b728e85ad3c469afacef6e57e8cb818
Samuel Yin <wsgddjy@live.com>
2025-04-02 10:47:07 +0800

    add message indicating feature branch in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <div className='flex flex-col justify-center items-center min-h-screen bg-gray-100'>
         <p>this is a demo to test jenkins</p>
         <p>do something in develop branch </p>
+        <p>I&#39;m a feature branch</p>
       </div>
     </>
   )
