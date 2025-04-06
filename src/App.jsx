a1725d0afc494025cc26afea9db6ead0dd133477
PomiHD <wsgddjy@live.com>
2025-04-06 08:39:00 +0000

    Jenkins: Auto-fix lint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 
 function App() {
-  const name = 'John';
+  const name = 'John'
   return (
     <>
       <div className='flex flex-col justify-center items-center min-h-screen bg-gray-100'>
         <p>this is a demo to test jenkins</p>
         <p>do something in develop branch </p>
         <p>I&#39;m a feature branch</p>
-<p>{name}</p><p>you should see change!</p>
+        <p>{name}</p>
+        <p>you should see change!</p>
         <p>a test!</p>
       </div>
     </>
